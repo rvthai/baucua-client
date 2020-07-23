9b60155d664b646c211087da01be2f95f5a64b03
Jackie Luu <jackiedl@Jackies-MacBook-Air.local>
2020-07-23 11:30:07 -0700

    Add basic game front-end

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,18 +1,40 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
+import {Link} from "react-router-dom";
 
-class Game extends Component{
-  constructor(props){
-    super(props);
-    this.state = {
+function Game(props){
+  const [socket] = useState(props.socket);
+  const [gamestate, setGamestate] = useState(props.gamestate);
 
-    }
+  useEffect(()=>{
+    socket.on("gamestate", ({gamestate}) => {
+      setGamestate(gamestate);
+    })
+    //eslint-disable-next-line
+  }, [])
+
+  const bet = (event) =>{
+
+    socket.emit("bet", ({room:gamestate.room, id:socket.id, amount:10, animal:event.target.value}));
   }
-  render(){
-    console.log(this.props.socket);
-    return(
-      <h1> Game </h1>
-    )
+  const roll = () => {
+    socket.emit("roll", ({room: gamestate.room}));
   }
+
+  return(
+    <div>
+      {console.log(gamestate)}
+      <button onClick={bet} value="deer">Deer</button>
+      <button onClick={bet} value="bau">Bau</button>
+      <button onClick={bet} value="chicken">Chicken</button>
+      <button onClick={bet} value="fish">Fish</button>
+      <button onClick={bet} value="crab">Crab</button>
+      <button onClick={bet} value="shrimp">Shrimp</button>
+      {props.host ? <button onClick={roll}>Roll</button> : null}
+      <Link to="/"><button>leave</button></Link>
+    </div>
+
+  )
 }
+
 
 export default Game;
